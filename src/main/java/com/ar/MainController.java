97e27acba31b925ee2551b9d5e0ec06715ee92a6
Artak Dashyan <a.dashyan@gmail.com>
2015-02-26 16:40:24 -0500

    populate db comboboxes

--- src/main/java/com/ar/MainController.java
+++ src/main/java/com/ar/MainController.java
@@ -4,9 +4,15 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
 import javafx.stage.Stage;
 
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 
@@ -14,6 +20,17 @@ import java.util.ResourceBundle;
  * Created by ar on 2/19/15.
  */
 public class MainController implements Initializable {
+
+    private Connector conn = null;
+
+    @FXML
+    private ComboBox from;
+    @FXML
+    private ComboBox to;
+
+
+    @FXML
+    private CheckBox dbbackup;
 
     @FXML
     private Button cancel;
@@ -69,17 +86,16 @@ public class MainController implements Initializable {
 
     }
 
-    public void setDefaults(String st){
-        System.out.print(st);
-//        toHost.setText(GetPropertyValues.getProperty(toHost.getId()));
-//        toPort.setText(GetPropertyValues.getProperty(toPort.getId()));
-//        toUser.setText(GetPropertyValues.getProperty(toUser.getId()));
-//        toPass.setText(GetPropertyValues.getProperty(toPass.getId()));
-//
-//        fromHost.setText(GetPropertyValues.getProperty(fromHost.getId()));
-//        fromPort.setText(GetPropertyValues.getProperty(fromPort.getId()));
-//        fromUser.setText(GetPropertyValues.getProperty(fromUser.getId()));
-//        fromPass.setText(GetPropertyValues.getProperty(fromPass.getId()));
+    public void setDefaults(){
+        if("checked".equals(GetPropertyValues.getProperty("dbbackup"))){
+            dbbackup.setSelected(true);
+        }
+        addDbListToElement(to);
+        addDbListToElement(from);
+    }
+
+    public void setConn(Connector conn) {
+        this.conn = conn;
     }
 
 
@@ -100,5 +116,36 @@ public class MainController implements Initializable {
 //        propsTo.setProperty("pass", fromPass.getText());
 //        return propsTo;
 //    }
+
+
+    private void addDbListToElement(ComboBox comboBox) {
+
+        Connection connection = null;
+
+        switch (comboBox.getId()) {
+            case "from":
+                connection = conn.connFrom;
+                break;
+            case "to":
+                connection = conn.connTo;
+                break;
+        }
+
+        try {
+            ResultSet rs = connection.getMetaData().getCatalogs();
+            while (rs.next()) {
+                if("information_schema".equals(rs.getString("TABLE_CAT")))
+                    continue;
+                if("mysql".equals(rs.getString("TABLE_CAT")))
+                    continue;
+                if("performance_schema".equals(rs.getString("TABLE_CAT")))
+                    continue;
+                comboBox.getItems().addAll(rs.getString("TABLE_CAT"));
+            }
+            comboBox.getSelectionModel().select(0);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
